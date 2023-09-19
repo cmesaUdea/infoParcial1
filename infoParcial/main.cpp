c766d2419c86ada9f11007a72d736ed9d715978f
josemesa0112 <josemesaenero8@gmail.com>
2023-09-19 17:07:53 -0500

    Creacion del menu y llamado de las funciones

--- infoParcial/main.cpp
+++ infoParcial/main.cpp
@@ -5,4 +5,23 @@ void setup(){
     Serial.begin(9600);
 }
 
+void publik(){
+    Serial.println("***********");
+    Serial.println("Bienvenido");
+    Serial.println("1. Probar Leds");
+    Serial.println("2. Ingresar imagen (8 lineas de 1 y 0)");
+    Serial.println("3. Probar secuencias");
+}
 
+void loop(){
+    publik();
+    while (Serial.available() == 0) {}
+    data = Serial.read();
+    if(data == '1'){
+        verificacion();
+    } else if (data == '2'){
+        ingresarImagen();
+    } else if (data == '3'){
+        secuencias();
+    }
+}
