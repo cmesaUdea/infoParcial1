47a0a40b617facab53aa127780deb401da17ff38
cmesaUdea <carlos.mesar@udea.edu.co>
2023-09-20 18:51:24 -0500

    Se crea la funcion para encender y apagar los LEDS

--- infoParcial/main.cpp
+++ infoParcial/main.cpp
@@ -6,3 +6,87 @@ void setup(){
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void encederLeds(){
+    Serial.println("Probando leds...");
+    shiftOut(pinData, pinClock, LSBFIRST, 255); //shiftout envia datos a dispositivos externos
+    shiftOut(pinData, pinClock, LSBFIRST, 255); 
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    shiftOut(pinData, pinClock, LSBFIRST, 255);
+    
+    // Ya asignados los desplazamientos mando la señal digital
+  digitalWrite(pinLatch, HIGH);
+  digitalWrite(pinLatch, LOW);
+
+}
+
+void apagarLeds(){
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  shiftOut(pinData, pinClock, LSBFIRST, 0);
+  
+  // Ya asignados los desplazamientos mando la señal digital
+  digitalWrite(pinLatch, HIGH);
+  digitalWrite(pinLatch, LOW);
+}
+
+
