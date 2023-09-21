576bc607445033444cbca892d2e95457e0b143a6
josemesa0112 <josemesaenero8@gmail.com>
2023-09-21 18:21:56 -0500

    Desarrollando funcion de la x y la flecha

--- infoParcial/main.cpp
+++ infoParcial/main.cpp
@@ -32,3 +32,45 @@ void verificacion(){
     apagarLeds();
     delay(1000);
 }
+
+void secuenciaX() {
+    int valoresFilasLeds[BYTE_SIZE];
+    int valorLedIzq = 128;
+    int valorLedDer = 1;
+    int valor = 0;
+    for (int i = 0; i < 4; i++) {
+        valor = valorLedIzq + valorLedDer;
+        valoresFilasLeds[i] = valor; // Guardo la fila que va hacia arriba
+        valoresFilasLeds[(BYTE_SIZE - 1) - i] = valor; // Guardo la misma fila que va hacia abajo
+        valorLedIzq = valorLedIzq/2;
+        valorLedDer = valorLedDer*2;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        shiftOut(pinData, pinClock, LSBFIRST, valoresFilasLeds[i]);
+    }
+
+    // Ya asignados los desplazamientos mando la señal digital
+    digitalWrite(pinLatch, HIGH);
+    digitalWrite(pinLatch, LOW);
+    Serial.println("Mostrando patron 2...");
+}
+
+void secuenciaFlecha() {
+    byte valorInicial = B11110000;
+
+    for (int i = 0; i < 4; i++) {
+        shiftOut(pinData, pinClock, LSBFIRST, valorInicial);
+        if(i != 3){ // Para que se repita el patron del medio (no desplazo)
+            valorInicial = (valorInicial >> 1);
+        }
+    }
+    for (int i = 0; i < 4; i++) {
+        shiftOut(pinData, pinClock, LSBFIRST, valorInicial);
+        valorInicial = (valorInicial << 1);
+    }
+    // Ya asignados los desplazamientos mando la señal digital
+    digitalWrite(pinLatch, HIGH);
+    digitalWrite(pinLatch, LOW);
+    Serial.println("Mostrando patron 4...");
+}
